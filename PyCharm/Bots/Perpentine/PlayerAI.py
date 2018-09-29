62891bd24560aba49e825ff275492fcfc5b24d72
EricPapagiannis <eric.papagiannis@mail.utoronto.ca>
2018-09-29 19:15:06 -0400

    fixed error when trying to move when dead

--- PyCharm/Bots/Perpentine/PlayerAI.py
+++ PyCharm/Bots/Perpentine/PlayerAI.py
@@ -38,68 +38,69 @@ class PlayerAI:
         if friendly_unit.status == 'DISABLED':
             self.cornerCount = 0
             self.direction = None
-        if self.cornerCount == 0:
-            startpoints = []
-            topleft = world.path.get_shortest_path(friendly_unit.position, corners[0], friendly_unit.snake)[0]
-            topright = world.path.get_shortest_path(friendly_unit.position, corners[1], friendly_unit.snake)[0]
-            botleft = world.path.get_shortest_path(friendly_unit.position, corners[2], friendly_unit.snake)[0]
-            botright = world.path.get_shortest_path(friendly_unit.position, corners[3], friendly_unit.snake)[0]
-            startpoints.append(topleft)
-            startpoints.append(topright)
-            startpoints.append(botleft)
-            startpoints.append(botright)
-
-            dist = 9999999
-
-            for point in startpoints:
-                newdist = pathfinder.get_taxi_cab_distance(friendly_unit.position, point)
-                if newdist < dist:
-                    dist = newdist
-                    next_move = point
-            if next_move in corners:
-                self.cornerCount += 1
         else:
-            print(world.get_neighbours(friendly_unit.position))
-            if self.direction is None:
-                print("abba baab @@@@@@@@@@")
-                neighbours = world.get_neighbours(friendly_unit.position)
-                print(neighbours)
-                for direction, position in neighbours.items():
-                    if position not in friendly_unit.snake and not world.is_wall(position):
+            if self.cornerCount == 0:
+                startpoints = []
+                topleft = world.path.get_shortest_path(friendly_unit.position, corners[0], friendly_unit.snake)[0]
+                topright = world.path.get_shortest_path(friendly_unit.position, corners[1], friendly_unit.snake)[0]
+                botleft = world.path.get_shortest_path(friendly_unit.position, corners[2], friendly_unit.snake)[0]
+                botright = world.path.get_shortest_path(friendly_unit.position, corners[3], friendly_unit.snake)[0]
+                startpoints.append(topleft)
+                startpoints.append(topright)
+                startpoints.append(botleft)
+                startpoints.append(botright)
 
-                        print(direction)
-                        self.direction = direction
+                dist = 9999999
 
-            print(self.direction)
-            if self.direction == Direction.SOUTH:
-                next_move = world.path.get_shortest_path(friendly_unit.position, (friendly_unit.position[0], 28), [])[0]
-            if self.direction == Direction.NORTH:
-                next_move = world.path.get_shortest_path(friendly_unit.position, (friendly_unit.position[0], 1), [])[0]
-            if self.direction == Direction.EAST:
-                next_move = world.path.get_shortest_path(friendly_unit.position, (28, friendly_unit.position[1]), [])[0]
-            if self.direction == Direction.WEST:
-                next_move = world.path.get_shortest_path(friendly_unit.position, (1, friendly_unit.position[1]), [])[0]
-                neighbours = world.get_neighbours(friendly_unit.position)
-            if self.cornerCount == 4:
-                tileUtils = TileUtils(world, friendly_unit, None)
-                tile = tileUtils.get_closest_friendly_territory_from(friendly_unit.position, friendly_unit.snake)
-                next_move = \
-                world.path.get_shortest_path(friendly_unit.position, tile.position, friendly_unit.snake)[0]
+                for point in startpoints:
+                    newdist = pathfinder.get_taxi_cab_distance(friendly_unit.position, point)
+                    if newdist < dist:
+                        dist = newdist
+                        next_move = point
+                if next_move in corners:
+                    self.cornerCount += 1
+            else:
+                print(world.get_neighbours(friendly_unit.position))
+                if self.direction is None:
+                    print("abba baab @@@@@@@@@@")
+                    neighbours = world.get_neighbours(friendly_unit.position)
+                    print(neighbours)
+                    for direction, position in neighbours.items():
+                        if position not in friendly_unit.snake and not world.is_wall(position):
 
-            if next_move in corners:
-                print("FUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUCK")
+                            print(direction)
+                            self.direction = direction
+
+                print(self.direction)
+                if self.direction == Direction.SOUTH:
+                    next_move = world.path.get_shortest_path(friendly_unit.position, (friendly_unit.position[0], 28), [])[0]
+                if self.direction == Direction.NORTH:
+                    next_move = world.path.get_shortest_path(friendly_unit.position, (friendly_unit.position[0], 1), [])[0]
+                if self.direction == Direction.EAST:
+                    next_move = world.path.get_shortest_path(friendly_unit.position, (28, friendly_unit.position[1]), [])[0]
+                if self.direction == Direction.WEST:
+                    next_move = world.path.get_shortest_path(friendly_unit.position, (1, friendly_unit.position[1]), [])[0]
+                    neighbours = world.get_neighbours(friendly_unit.position)
+                if self.cornerCount == 4:
+                    tileUtils = TileUtils(world, friendly_unit, None)
+                    tile = tileUtils.get_closest_friendly_territory_from(friendly_unit.position, friendly_unit.snake)
+                    next_move = \
+                    world.path.get_shortest_path(friendly_unit.position, tile.position, friendly_unit.snake)[0]
+
+                if next_move in corners:
+                    print("FUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUCK")
+                    self.direction = None
+                    self.cornerCount += 1
+
+            if world.position_to_tile_map[next_move].is_friendly:
+                self.cornerCount = 0
                 self.direction = None
-                self.cornerCount += 1
-
-        if world.position_to_tile_map[next_move].is_friendly:
-            self.cornerCount = 0
-            self.direction = None
-        friendly_unit.move(next_move)
+            friendly_unit.move(next_move)
 
 
-        print("Turn {0}: currently at {1}, making {2} move to {3}.".format(
-            str(self.turn_count),
-            str(friendly_unit.position),
-            'outbound' if self.outbound else 'inbound',
-            str(next_move)
-        ))
+            print("Turn {0}: currently at {1}, making {2} move to {3}.".format(
+                str(self.turn_count),
+                str(friendly_unit.position),
+                'outbound' if self.outbound else 'inbound',
+                str(next_move)
+            ))
